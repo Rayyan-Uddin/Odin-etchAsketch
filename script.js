66bf29837f2977506343ac44c5ffb8b997f68852
Rayyan Uddin <rayyanuddin007@gmail.com>
2025-03-04 09:47:53 +0500

    -Add buttons 1.Clear 2.RGB 3.Sketch -Change UI

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+// GLOBAL VARIABLES
+let divDefaultColor = ' rgb(255, 255, 255)';
+let divHoverColor = ' black';
+let isRaibowColor = false;
+let isSketchOn = false;
+let gridSize = 16;
 
+// INPUT GRID SIZE
+function changeGrid() {
+
+    gridSize = +prompt("How many box (0-100) you need in a row:");
+    createGrid();
+}
+
+// CREATE GRID-BOXES TO GRIDSIZE * GRIDSIZE (default 16*16)
 function createGrid() {
-
-    let gridSize = +prompt("How many grids (0-100) you need in a row:")
-
     if (gridSize <= 100) {
         let containerSize = 400;
         let squareSize = containerSize / gridSize
@@ -18,12 +29,84 @@ function createGrid() {
     } else {
         alert("Enter a Number Less than 100.")
     }
+    defaultHover(); //call function to change color on hover
+}
+createGrid();
+// 
+// CHANGE COLOR ON HOVER
+function defaultHover() {
+    clearGrid()
+
     let divs = document.querySelectorAll('.divs');
     divs.forEach(divs => {
+        divs.innerHTML = ''
         divs.addEventListener('mouseover', () => {
-            divs.style.backgroundColor = 'rgb(1, 115, 255)'
+            divs.style.backgroundColor = divHoverColor;
+
         })
     })
 }
+
+// CHANGE GRID COLOR TO DEFAULT
+function clearGrid() {
+    document.querySelectorAll('.divs').forEach(div => {
+        div.style.opacity = '1';
+        div.style.backgroundColor = divDefaultColor;
+    })
+}
+
+// CHANGE MULTI COLOR ON HOVER
+function changeToRainbow() {
+    clearGrid()
+    let divs = document.querySelectorAll('.divs');
+    divs.forEach(divs => {
+        divs.addEventListener('mouseover', () => {
+            const letters = "0123456789ABCDEF";
+            let color = "#";
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            divs.style.backgroundColor = color;
+
+        })
+    })
+
+}
+
+
+
+// MAKE COLOR DARKER ON HOVER
+function changeToSketch() {
+    clearGrid();
+    let divs = document.querySelectorAll('.divs');
+    divs.forEach(div => {
+        div.style.opacity = '0';
+        div.addEventListener('mouseover', () => {
+            let currentOpacity = parseFloat(div.style.opacity);
+            if (currentOpacity < 1) {
+                div.style.opacity = (currentOpacity + 0.1).toString(); // Increase opacity by 0.1
+            };
+
+        })
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
